cdd61f8033c9ec8075c7f85ed517167aa4a27161
Joshua Morris <jmorris252@students.sbts.edu>
2018-11-27 15:57:54 -0600

    Adding Styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,37 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import styled from 'styled-components'
+
+const AppWindow = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`
+
+const VerticalSpace = styled.div`
+  margin: 25px;
+`
+
+const AppBackground = styled.div`
+  background-color: seagreen;
+  width: 25%;
+`
 
 const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
+  <AppWindow>
+    <AppBackground>
+      <VerticalSpace />
+      <AppWindow>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </AppWindow>
+      <VerticalSpace />
+    </AppBackground>
+  </AppWindow>
 )
 
 export default App
